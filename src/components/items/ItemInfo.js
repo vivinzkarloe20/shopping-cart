9c83c6e1822d3ba0e7e3645a2b2bbc4242fc7487
Vinz <vinzkarloemendoza@gmail.com>
2019-05-28 20:31:15 +0800

    Added route to go to specific item information when the image is clicked

--- src/components/items/ItemInfo.js
+++ src/components/items/ItemInfo.js
@@ -1,24 +1,92 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
+import { withStyles } from '@material-ui/core/styles';
+import { Button, Grid, MenuItem, TextField, Typography } from '@material-ui/core';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import * as actionCreators from '../../store/actions/index';
+
+
+const styles = theme => ({
+    wrapper: {
+        // marginLeft: '15%',
+        // marginRight: '15%',
+        marginTop: 'auto',
+        display: 'flex',
+        justifyContent: 'center',
+    }
+}); 
+
+const qty = [];
+
+for(let i = 1; i <= 5; i++){
+    qty.push({label: i, value: i});
+}
 
 class ItemInfo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            amount: 0,
+        }
+    }
+
     render() {
-        // this.setState({
-        //     itemId: item
-        // });
-        const { item } = this.props
+        const { item, classes } = this.props
+
+        const handleChange = e => {
+            this.setState({
+                amount: e.target.value
+            })
+        }
 
         const itemInfo = item ? (
             <div>
-                <p>{item.name}</p>
-                <p>${item.price}</p>
+                <Typography>{item.name}</Typography>
+                <Typography>${item.price}</Typography>
+                <hr/>
+                <TextField
+                    select
+                    id="outlined-adornment-amount"
+                    className={classes.textField}
+                    variant="outlined"
+                    label="Qty"
+                    value={this.state.amount}
+                    onChange={handleChange}
+                >
+                    {qty.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <Button size="small" color="primary" onClick={() => {
+                    this.props.onAddItemToCart(item, this.state.amount);
+                    this.setState({
+                        amount: 0,
+                    })
+                    }}>
+                    <ShoppingCart/>
+                    Add to Cart
+                </Button>
+               
             </div>
             
         ) : (<p>none</p>);
         return (
-            <div>
-                {itemInfo}
-            </div>
+            <React.Fragment>
+                 <Link to="/">
+                    <Typography>
+                    <ArrowBack/>
+                        Return to Items
+                    </Typography>
+                </Link>
+                <Grid container spacing={40} className={classes.wrapper}>
+                    {itemInfo}
+                </Grid>
+            </React.Fragment>
         )
     }
 }
@@ -30,4 +98,8 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemInfo);
+const mapDispatchToProps = dispatch => ({
+    onAddItemToCart: (item, itemQty) => dispatch(actionCreators.addToCart({item, qty: itemQty})),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(ItemInfo));
